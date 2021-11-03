491dfedf82b6596cb24823e5b5bef143375cba58
ujin-Bang <qkddnwls14@gmail.com>
2021-11-03 13:21:50 +0900

    어댑터를 화면에서 사용할 준비 +리스트뷰의 어댑터로 우리가 만든 어댑터 연결

--- app/src/main/java/com/neppplus/listview_20211103/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20211103/MainActivity.kt
@@ -2,11 +2,14 @@ package com.neppplus.listview_20211103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listview_20211103.adapters.StudentAdapter
 import com.neppplus.listview_20211103.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
 
     val mStudentList = ArrayList<StudentData>()
+    lateinit var mStudentAdapter: StudentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,8 +19,11 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add( StudentData( "김준기",1996, "경기도 남양주시" ))
         mStudentList.add( StudentData( "방우진",1983, "경기도 고양시" ))
         mStudentList.add( StudentData( "이아현",1996, "서울시 동대문구" ))
-        mStudentList.add( StudentData( "이지원",1993, "서울시 동대문구" ))
+        mStudentList.add( StudentData( "이지원",1993, "서울시 관악구" ))
+        mStudentList.add( StudentData( "이지원",1993, "서울시 관악구" ))
+        mStudentList.add( StudentData( "이지원",1993, "서울시 관악구" ))
 
-
+        mStudentAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+        studentListView.adater = mStudentAdapter
     }
 }
